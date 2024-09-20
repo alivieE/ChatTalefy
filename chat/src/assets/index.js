0933eda2cf86759ffdc063541b6340c0490230e7
alivieE <153372860+alivieE@users.noreply.github.com>
2024-09-20 21:52:19 +0300

    added styles fro user and styles for scroll-bar

--- chat/src/assets/index.js
+++ chat/src/assets/index.js
@@ -2,7 +2,8 @@ import close from './close.svg'
 import info from './info.svg'
 import davinchi from './Davinchi.jpg'
 import arrowUp from './ArrowUp.svg'
+import typing from './type.svg'
 
-const img = {close,info,davinchi,arrowUp}
+const img = {close,info,davinchi,arrowUp,typing}
 
 export default img
--- chat/src/components/ChatSpace/Chatspace.jsx
+++ chat/src/components/ChatSpace/Chatspace.jsx
@@ -1,43 +1,62 @@
-import React from 'react'
-import s from './Chatspace.module.css'
-import images from '../../assets/index.js'
+import React, { useState, useEffect } from 'react';
+import s from './Chatspace.module.css';
+import typing from '../../assets/type.svg'
 
-const Chatspace = ({messages}) => {
-  
+const Chatspace = ({ messages }) => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      setLoading(true);
+      const timer = setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [messages]);
+
   return (
     <section className={s.section}>
       <ul className={s.messagesList}>
         <li className={s.messagesListItem}>
           <div className={s.heroAnswer}>
-            <div className={s.heroImage} ></div>
+            <div className={s.heroImage}></div>
             <div className={s.herotext}>
               <p className={s.heroName}>Davinchi</p>
-              <p className={s.heroMessageText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, illo? Dolore labore illum esse ducimus. Ab deleniti in fugiat exercitationem?</p>
+              <p className={s.heroMessageText}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, illo? Dolore labore illum esse ducimus. Ab deleniti in fugiat exercitationem?
+              </p>
             </div>
           </div>
         </li>
-        
-          {messages.map((message, index) => (
-            <li key={index} className={s.heroMessageText}>              
-              <div className={s.userAsk}><p className={s.userName}>You</p>{message}</div>              
 
-              
-              <div className={s.heroAnswer}>
-              <div className={s.heroImage} ></div>
-              <div className={s.herotext}>
-                <p className={s.heroName}>You</p>
-                <p className={s.heroMessageText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, illo? Dolore labore illum esse ducimus. Ab deleniti in fugiat exercitationem?</p>
+        {messages.map((message, index) => (
+          <li key={index} className={s.userMessageText}>
+            <div className={s.userAsk}>
+              <p className={s.userName}>You</p>
+              {message}
+            </div>
+            {loading && index === messages.length - 1 ? ( 
+              <div className={s.loadingContainer}>
+                <img src={typing} alt="Loading..." className={s.loadingAnimation} />                
               </div>
-          </div>
-            </li>
-          ))}
+            ) : (
+              <div className={s.heroAnswer}>
+                <div className={s.heroImage}></div>
+                <div className={s.herotext}>
+                  <p className={s.userName}>You</p>
+                  <p className={s.heroMessageText}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, illo? Dolore labore illum esse ducimus. Ab deleniti in fugiat exercitationem?
+                  </p>
+                </div>
+              </div>
+            )}
+          </li>
+        ))}
       </ul>
-      <div>
-          
-        </div>
     </section>
-    
-  )
-}
+  );
+};
 
-export default Chatspace
+export default Chatspace;
