398951ad2c51b4f7c1af9e5fe5afdc359998fa54
alivieE <153372860+alivieE@users.noreply.github.com>
2024-09-27 22:14:19 +0300

    fix

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Chat from './components/Chat/Chat'
-import Chatspace from './components/ChatSpace/Chatspace'
+
 
 const App = () => {
   return (
-    
     <Chat></Chat>
   )
 }
